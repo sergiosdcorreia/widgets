b9c088cf4380c6057ce4708b709ef0dc1f8b6292
sergiosdcorreia <sergiosdcorreia@gmail.com>
2018-08-23 15:36:24 +0100

    • Add title and favicon. • Add image to large widget.

--- src/components/widgets/widgetLarge/WidgetLarge.js
+++ src/components/widgets/widgetLarge/WidgetLarge.js
@@ -1,11 +1,11 @@
 import React from 'react';
-
+import ImageBlog from '../../../Assets/images/blog-img01.jpg';
 
 const WidgetLarge = () => (
     <div className="widget-lg">
         <a className="link" href="https://www.funeralzone.co.uk/">
             <div className="img--container">
-                <img src="https://via.placeholder.com/500x500" alt="widget" />
+                <img src={ImageBlog} alt="widget" />
             </div>
             <div className="text--block">
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
