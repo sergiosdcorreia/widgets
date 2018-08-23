33a5ebdcced4e26926d4eb432c4e20336c606542
sergiosdcorreia <sergiosdcorreia@gmail.com>
2018-08-23 14:56:11 +0100

    • Add styles files. • Add header, footer and widget components

---
+++ src/components/header/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Header = () => (
+
+    <div className="header">
+        <div className="container">
+            <div className="fz-logo-color">
+                <img src="https://www.funeralzone.co.uk/assets/img/core/logo_hr_blue_svg.svg" alt="Funeral Zone Ltd" />
+            </div>
+        </div>
+    </div>
+
+)
+
+export default Header;
---
+++ src/components/widgets/widgetLarge/WidgetLarge.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+const WidgetLarge = () => (
+    <div className="widget-lg">
+        <a className="link" href="https://www.funeralzone.co.uk/">
+            <div className="img--container">
+                <img src="https://via.placeholder.com/500x500" alt="widget" />
+            </div>
+            <div className="text--block">
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            </div>
+        </a>
+    </div>
+)
+
+export default WidgetLarge;
