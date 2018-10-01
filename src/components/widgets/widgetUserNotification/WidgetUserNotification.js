4059ad0e3209094daa500e189fcf05aadae61ff5
sergiosdcorreia <sergiosdcorreia@gmail.com>
2018-10-01 12:09:11 +0100

    • Added icon for User Notification component when there's no image

--- src/components/widgets/widgetUserNotification/WidgetUserNotification.js
+++ src/components/widgets/widgetUserNotification/WidgetUserNotification.js
@@ -1,18 +1,27 @@
 import React from 'react';
 import './WidgetUserNotification.css';
 
+import IconUserNotification from '../../icons/IconUserNotification';
+
 const WidgetUserNotification = props => {
+
+    const { image, title, description } = props;
 
     return (
         <div className="widget-user-notification">
             <div className="widget-container">
                 <div className="flex-notification">
                     <div className="img--container">
-                        <img src={props.image} alt={props.title} />
+                        {
+                            image && <img src={image} alt="" />
+                        }
+                        {
+                            image.length === 0 && <IconUserNotification />
+                        }
                     </div>
                     <div className="text--block">
-                        <p className="title">{props.title}</p>
-                        <p className="description">{props.description}</p>
+                        <p className="title">{title}</p>
+                        <p className="description">{description}</p>
                     </div>
                 </div>
                 {/* <div className="card__actions">
